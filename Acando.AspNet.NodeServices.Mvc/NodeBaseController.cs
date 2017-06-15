f66c9ce1d0031c5bd91dafc75777b728fc3319af
Barlind <tobias@barlind.se>
2017-06-15 13:25:16 +0200

    Added custom entry file path.

--- Acando.AspNet.NodeServices.Mvc/NodeBaseController.cs
+++ Acando.AspNet.NodeServices.Mvc/NodeBaseController.cs
@@ -25,16 +25,66 @@ namespace Acando.AspNet.NodeServices.Mvc
             _nodeServerEntryFilePath = ConfigurationManager.AppSettings["NodeServerEntryFilePath"] ?? "./react.server";
         }
 
-        public async Task<NodeResult> NodeResultAsync(params string[] paramsStrings)
+        /// <summary>
+        /// Returns a NodeResult with the input params as input to the node method
+        /// </summary>
+        /// <param name="paramsObjects"></param>
+        /// <returns></returns>
+        protected async Task<NodeResult> NodeResultAsync(params object[] paramsObjects)
         {
-            var result = await _nodeServices.InvokeAsync<string>(_nodeServerEntryFilePath, HttpContext.Request.Url?.LocalPath ?? "/", paramsStrings);
+            string[] jsonObjects = new string[paramsObjects.Length];
+            
+            for (int i = 0; i < paramsObjects.Length ; i++)
+            {
+                // object is most likely already serialized to json
+                if (paramsObjects[i] is string)
+                {
+                    jsonObjects[i] = paramsObjects[i].ToString();
+                }
+                else
+                {
+                    jsonObjects[i] = JsonConvert.SerializeObject(paramsObjects[i], _serializerSettings);
+                }
+            }
+            
+            var result = await _nodeServices.InvokeAsync<string>(_nodeServerEntryFilePath, HttpContext.Request.Url?.LocalPath ?? "/", jsonObjects);
             return new NodeResult(result);
         }
 
-        protected async Task<NodeResult> NodeResultAsync(params object[] paramsObjects)
+        /// <summary>
+        /// Returns a NodeResult based on a custom NodeServerEntryFilePath
+        /// </summary>
+        /// <param name="paramsObjects">First object must be a string containing NodeServerEntryFilePath</param>
+        /// <returns></returns>
+        protected async Task<NodeResult> NodeResultEntryFileAsync(params object[] paramsObjects)
         {
-            var result = await _nodeServices.InvokeAsync<string>(_nodeServerEntryFilePath, HttpContext.Request.Url?.LocalPath ?? "/", paramsObjects.Select(x => JsonConvert.SerializeObject(x, _serializerSettings)).ToArray());
+            string[] jsonObjects = new string[paramsObjects.Length];
+
+            if (paramsObjects.Length < 1)
+            {
+                throw new ArgumentException("Expected at least one object in params.");
+            }
+
+            if (!(paramsObjects[0] is string))
+            {
+                throw new ArgumentException("First param must be of type string and contain a NodeServerEntryFilePath ");
+            }
+
+            for (int i = 1; i < paramsObjects.Length; i++)
+            {
+                if (paramsObjects[i] is string)
+                {
+                    // object is most likely already serialized
+                    jsonObjects[i-1] = paramsObjects[i].ToString();
+                }
+                else
+                {
+                    jsonObjects[i-1] = JsonConvert.SerializeObject(paramsObjects[i], _serializerSettings);
+                }
+            }
+
+            var result = await _nodeServices.InvokeAsync<string>(paramsObjects[0].ToString(), HttpContext.Request.Url?.LocalPath ?? "/", jsonObjects);
             return new NodeResult(result);
-        }
+        }        
     }
 }
